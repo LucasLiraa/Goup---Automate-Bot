c2c7c9ca226afbadbd9fb43a08b6d33e96d01aa5
Lucas Lira <67843377+LucasLiraa@users.noreply.github.com>
2024-07-22 15:08:57 -0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -15,7 +15,7 @@ def verificSerial(mensagem):
 # Consulta a planilha na rede em busca do número de série
 def consultDados(Serial):
     try:
-        df = pd.read_excel(r"\\10.111.200.59\Controle\Estoque\Relatório Personalizado 14-05-2024.xlsx")
+        df = pd.read_excel(r"Z:\Relatório Personalizado 14-05-2024.xlsx")  # Usando o caminho da unidade mapeada
         if Serial:
             resultado = df[df['NÚMERO DO SERIAL'] == Serial]
             if not resultado.empty:
@@ -46,9 +46,9 @@ def adicDados(NomeDispositivo, ModeloDispositivo, SerialDispositivo, Processador
             'TipoDispositivo': [TipoDispositivo]  # Nova coluna para armazenar o tipo do dispositivo
         })
         
-        df = pd.read_excel(r"\\10.111.200.59\Controle\Estoque\Estoque atual.xlsx")
+        df = pd.read_excel(r"Z:\Estoque atual.xlsx")  # Usando o caminho da unidade mapeada
         df = pd.concat([df, nova_linha], ignore_index=True)
-        df.to_excel(r"\\10.111.200.59\Controle\Estoque\Estoque atual.xlsx", index=False)
+        df.to_excel(r"Z:\Estoque atual.xlsx", index=False)  # Usando o caminho da unidade mapeada
         return "Boa bixo! Equipamento adicionado no estoque."
     except Exception as e:
         return f"Erro ao adicionar nova linha à planilha de estoque: {e}"
@@ -72,14 +72,14 @@ def exclDados(mensagem):
                 id_dispositivo = st.text_input(f"Insira o ID do {tipo} a ser excluído:", key=f"ID_{tipo}")
                 if id_dispositivo:
                     # Carrega a planilha de estoque
-                    df = pd.read_excel(r"\\10.111.200.59\Controle\Estoque\Estoque atual.xlsx")
+                    df = pd.read_excel(r"Z:\Estoque atual.xlsx")  # Usando o caminho da unidade mapeada
 
                     # Verifica se o ID está na planilha para o tipo de dispositivo especificado
                     if id_dispositivo in df[df['TipoDispositivo'] == tipo]['SerialDispositivo'].values:
                         # Remove a linha correspondente ao ID
                         df = df[(df['TipoDispositivo'] != tipo) | (df['SerialDispositivo'] != id_dispositivo)]
                         # Salva as alterações na planilha
-                        df.to_excel(r"\\10.111.200.59\Controle\Estoque\Estoque atual.xlsx", index=False)
+                        df.to_excel(r"Z:\Estoque atual.xlsx", index=False)  # Usando o caminho da unidade mapeada
 
                         return f"Boa! {tipo} com ID {id_dispositivo} removido do estoque."
                     else:
@@ -92,7 +92,7 @@ def exclDados(mensagem):
 # Verifica o número de itens para um modelo específico
 def verificarNumeroItensModelo(ModeloDispositivo):
     try:
-        df = pd.read_excel(r"\\10.111.200.59\Controle\Estoque\Estoque atual.xlsx")
+        df = pd.read_excel(r"Z:\Estoque atual.xlsx")  # Usando o caminho da unidade mapeada
         if ModeloDispositivo:
             num_itens = df[df['ModeloDispositivo'] == ModeloDispositivo].shape[0]
             return num_itens
